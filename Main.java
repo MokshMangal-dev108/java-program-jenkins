81f846e1e64dc983863850004db186ff2ab0fa9b
Moksh Mangal <106534848+MokshMangal-dev108@users.noreply.github.com>
2024-07-17 01:43:27 +0530

    done

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 import java.util.Date;
+import java.util.Random;
 
 public class Main{
     public static void main(String[] args) {
         System.out.println("this is output from java project");
         System.out.println("Hello");
         System.out.println(new Date());
+        System.out.println(new Random());
     }
 }
